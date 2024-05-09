107de8ec4adee65ac292933fda7f7a359eee8b5c
Jonny Tran <146238514+JTran-IDM@users.noreply.github.com>
2024-05-09 14:11:43 -0700

    initial extralit server setup

---
+++ src/extralit/server/context/datasets.py
@@ -0,0 +1,20 @@
+import os
+from typing import Optional
+
+import argilla as rg
+from argilla.client.feedback.dataset.remote.dataset import RemoteFeedbackDataset
+from argilla.client.sdk.commons.errors import UnauthorizedApiError
+
+
+def get_argilla_dataset(name="Table-Preprocessing", workspace="itn-recalibration") -> RemoteFeedbackDataset:
+    try:
+        rg.init(
+            api_url=os.getenv('ARGILLA_BASE_URL'),
+            api_key=os.getenv('ARGILLA_API_KEY'),
+        )
+    except Exception as e:
+        print(e)
+
+    dataset = rg.FeedbackDataset.from_argilla(name=name, workspace=workspace)
+
+    return dataset
