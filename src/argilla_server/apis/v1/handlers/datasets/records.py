515f0760cd27b0b1d3d71860582075bdb88ac35d
Jonny Tran <146238514+JTran-IDM@users.noreply.github.com>
2024-06-10 16:49:23 -0700

    latest

--- src/argilla_server/apis/v1/handlers/datasets/records.py
+++ src/argilla_server/apis/v1/handlers/datasets/records.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import logging
 import re
 from typing import Dict, List, Optional, Tuple, Union
 from uuid import UUID
